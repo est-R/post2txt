b2dab92c642baab45a00efa206a99c159bd5fc1e
est-R <gr4wer@gmail.com>
2023-05-24 03:56:09 +0300

    Base vk functionality. The newly loaded posts doesn't work though.

--- mainscript.js
+++ mainscript.js
@@ -8,16 +8,17 @@ const CSS_MAP = getCssMap();
 
 // MainApp
 // const throttledInject = throttle(btn_inject_base, 200);
-
-// document.addEventListener("DOMContentLoaded", () => {document.querySelectorAll('.' + CSS_MAP.mainFeed[0])[0].addEventListener('DOMSubtreeModified', btn_inject_base);});
+// if (location.host + location.pathname == "vk.com/feed") { btnFeedClass = 0 }
+// else { btnFeedClass = 1 };
+document.addEventListener("DOMContentLoaded", () => {document.querySelectorAll(CSS_MAP.mainFeed[0])[0].addEventListener('DOMSubtreeModified', throttle(btn_inject_base, 200))});
 // document.addEventListener("DOMContentLoaded", btn_inject_base);
-document.querySelectorAll('.' + CSS_MAP.mainFeed[0])[0].addEventListener('DOMSubtreeModified', throttle(btn_inject_base, 200));
+// document.querySelectorAll('.' + CSS_MAP.mainFeed[0])[0].addEventListener('DOMSubtreeModified', throttle(btn_inject_base, 200));
 btn_inject_base();
 
 
 // ButtonInjection
 function btn_inject_base() {
-    let posts = document.querySelectorAll('.' + CSS_MAP.post);
+    let posts = document.querySelectorAll('.' + CSS_MAP.post[0]);
 
     switch (ACTIVE_SITE) {
         case ('vk.com'):
@@ -36,20 +37,30 @@ function btn_inject_base() {
 // Button injection
 
 function inject_vk(post) {
-    // 4 if there is no '-'
-    let postID = post.id.replace(/[^\d_]/g, '');
-    let btnID = "postsaver_btn-" + postID;
-    if (post.getElementsByClassName("postsaver_btn")[0]) { return; }
+    post.setAttribute('postsaver_id:', Math.random().toString(16).slice(2)) // Generate custom post id
+    var postText = post.querySelectorAll('.' + CSS_MAP.postText)[0];
+    if (postText)
+    {
+        postText.classList.add('postsaver_postText');
+    }
 
-    post.querySelectorAll(".ui_actions_menu")[0].innerHTML += '<a class="ui_actions_menu_item postsaver_btn ' + btnID + '">Скачать пост</a>';
-    post.querySelectorAll(".postsaver_btn")[0].addEventListener("click", btn_save);
+    var buttonContainer = post.querySelectorAll('.' + CSS_MAP.buttonContainer)[0]; // [2], because anything in footer of a post has the same class
+    if (buttonContainer == null) {
+        return;
+    }
+
+    var btn = document.createElement('a');
+    btn.innerHTML += 'Скачать пост';
+    btn.classList.add(CSS_MAP.buttonStyle);
+    btn.classList.add('postsaver_btn');
+    buttonContainer.appendChild(btn);
+    buttonContainer.querySelectorAll(".postsaver_btn")[0].addEventListener("click", btn_save);
 }
 
 function inject_tumblr(post) {
     post.setAttribute('postsaver_id:', Math.random().toString(16).slice(2)) // Generate custom post id
     var buttonContainer = post.querySelectorAll('.' + CSS_MAP.buttonContainer)[2]; // [2], because anything in footer of a post has the same class
-    if (buttonContainer == null)
-    {
+    if (buttonContainer == null) {
         return;
     }
 
@@ -58,12 +69,12 @@ function inject_tumblr(post) {
     btn.classList.add(CSS_MAP.buttonStyle);
     btn.classList.add('postsaver_btn');
     buttonContainer.appendChild(btn);
-    buttonContainer.querySelectorAll(".postsaver_btn")[0].addEventListener("click", btn_save); 
+    buttonContainer.querySelectorAll(".postsaver_btn")[0].addEventListener("click", btn_save);
 }
 
 
-function btn_save(post) {
-    var postText = this.innerHTML;
+function btn_save() {
+    var postText = this.closest('.' + CSS_MAP.post[0]).querySelectorAll('.' + CSS_MAP.postText)[0].innerHTML;
     postText = processText(postText);
     alert(postText);
     //save string as file
@@ -71,7 +82,7 @@ function btn_save(post) {
     let blob = new Blob([postText], { type: 'text/plain' });
     let url = URL.createObjectURL(blob);
     a.setAttribute('href', url);
-    a.setAttribute('download', postID);
+    a.setAttribute('download', this);
     a.click();
 }
 
@@ -107,14 +118,14 @@ function getCssMap() {
             break;
         case "vk.com":
             css_map = {
-                mainFeed: ['feed_rows'],
-                post: 'wall_post_text',
+                mainFeed: ['feed_rows', 'page_wall_posts'],
+                post: ['_post', 'post'],
                 img: ['RoN4R', 'tPU70', 'xhGbM'],
                 imgSource: 'eqBap',
-                buttonContainer: 'MCavR',
-                buttonStyle: 'sfGru',
-                buttonWrapper: ['tOKgq', 'eIaSl'],
-                tags: 'hAFp3',
+                buttonContainer: 'ui_actions_menu',
+                buttonStyle: 'ui_actions_menu_item',
+                buttonWrapper: ['ui_actions_menu_wrap'],
+                postContent: '_post_content',
                 postText: 'wall_post_text'
             };
             break;
@@ -127,23 +138,23 @@ function getCssMap() {
 function throttle(func, delay) {
     let timeoutId;
     let lastExecutedTime = 0;
-    
-    return function(...args) {
-    const currentTime = Date.now();
-    
-    if (currentTime - lastExecutedTime >= delay) {
-    func.apply(this, args);
-    lastExecutedTime = currentTime;
-    }
-    else {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => {
-    func.apply(this, args);
-    lastExecutedTime = Date.now();
-    }, delay);
-    }
+
+    return function (...args) {
+        const currentTime = Date.now();
+
+        if (currentTime - lastExecutedTime >= delay) {
+            func.apply(this, args);
+            lastExecutedTime = currentTime;
+        }
+        else {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                func.apply(this, args);
+                lastExecutedTime = Date.now();
+            }, delay);
+        }
     };
-    }
+}
 
 //TODO: Track page change? When URL change https://vk.com/feed resetr first_scan | FOR VK
 // Tumblrr, Facebook, blogpost, twitter, instagramm
